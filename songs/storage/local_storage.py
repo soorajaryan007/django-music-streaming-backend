0b7455a359a593e2e31dd4792cbe6a0b77f70aca
sooraj aryan <soorajaryan007@gmail.com>
2026-03-22 08:08:29 +0530

    refactor

--- songs/storage/local_storage.py
+++ songs/storage/local_storage.py
@@ -10,12 +10,12 @@ class LocalStorage:
         os.makedirs(self.upload_folder, exist_ok=True)
 
     def save_audio_file(self, file):
-
         filename = f"{uuid.uuid4()}.mp3"
-
         file_path = os.path.join(self.upload_folder, filename)
 
-        file.save(file_path)
+        with open(file_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
 
         return file_path
-    
+        
